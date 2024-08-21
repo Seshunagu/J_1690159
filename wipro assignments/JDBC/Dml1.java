c2f483b3c2d7a97d87f60a0d187a90174e2f02d9
Seshunagu <146971128+Seshunagu@users.noreply.github.com>
2024-08-21 13:51:43 +0530

    Update Dml1.java

--- wipro assignments/JDBC/Dml1.java
+++ wipro assignments/JDBC/Dml1.java
@@ -4,7 +4,7 @@ class Dml1{
 try{
  Class.forName("com.mysql.cj.jdbc.Driver");
 System.out.println("Driver loaded without any interpections");
-Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/seshu","seshunagu","Seshu@1608$");
+Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/your_database_name","your_database_username","your_database_password");
 System.out.println("Connected to the database successfully");
 
 Statement st=c.createStatement();
